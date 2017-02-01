52389f0d302c18fc6199471205985fd9390027a6
Ololoshechkin <v.brilyantov@yandex.ru>
2017-02-01 23:54:03 +0300

    - some consolle interactions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import javafx.util.Pair;
 
+import javax.xml.bind.ValidationEvent;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
@@ -10,6 +11,8 @@ import java.util.Scanner;
  */
 public class Main {
 
+    public static int NumberOfClasses = 4;
+
     private static int getClass(double x, double y) {
         if (0.75 - x <= y && y <= 0.92 - x) return 3;
         if ((x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5) <= 0.1)
@@ -17,26 +20,29 @@ public class Main {
         return (y >= -(x - 0.5) * (x - 0.5) + 0.5 ? 1 : 0);
     }
 
-    private static int getNumberOfClasses() {
-        return 4;
-    }
-
-    private static int getStyddyingSamplesNumber() {
-        return 10000;
-    }
-
-    private static int getValidationSamplesNumber() {
-        return 7000;
-    }
-
     public static void main(String[] args) throws InterruptedException {
+        int StyddyingSamplesNumber = 4000;
+        int ValidationSamplesNumber = 7000;
+
         Scanner sc = new Scanner(System.in);
         Random rnd = new Random();
+
+        System.out.println("If you want default params, then enter '-', else enter another word: ");
+        if (sc.next().compareTo("-") != 0) {
+            System.out.println("Enter studdying samples number :");
+            StyddyingSamplesNumber = sc.nextInt();
+            if (StyddyingSamplesNumber < 10) StyddyingSamplesNumber = 10;
+            System.out.println("Enter validation samples number :");
+            ValidationSamplesNumber = sc.nextInt();
+            if (ValidationSamplesNumber < 10) ValidationSamplesNumber = 10;
+        }
+        System.out.println("wait, please...");
+
         DecisionTree decisionTree = new DecisionTree();
         decisionTree.setDimentions(2);
-        decisionTree.setNumberOfClasses(getNumberOfClasses());
+        decisionTree.setNumberOfClasses(NumberOfClasses);
         ArrayList<ObjectWithClass> points = new ArrayList<>();
-        for (int i = 0; i < getStyddyingSamplesNumber(); ++i) {
+        for (int i = 0; i < StyddyingSamplesNumber; ++i) {
             double x = rnd.nextDouble(), y = rnd.nextDouble();
             int classNmb = getClass(x, y);
             FeaturesVector curFeaturesVector = new FeaturesVector(2);
@@ -44,11 +50,10 @@ public class Main {
             curFeaturesVector.setFeature(1, y);
             points.add(new ObjectWithClass(curFeaturesVector, classNmb));
         }
-        System.out.println("wait, please...");
         decisionTree.studdy(points);
         decisionTree.visualizeTree(points);
         int goodAnswers = 0;
-        for (int i = 0; i < getValidationSamplesNumber(); ++i) {
+        for (int i = 0; i < ValidationSamplesNumber; ++i) {
             double x = rnd.nextDouble(), y = rnd.nextDouble();
             int expectedClassNmb = getClass(x, y);
             FeaturesVector curFeaturesVector = new FeaturesVector(2);
@@ -57,14 +62,14 @@ public class Main {
             int curAns = decisionTree.getClassByObject(curFeaturesVector);
             goodAnswers += (curAns == expectedClassNmb ? 1 : 0);
         }
-        System.out.println("correct : " + 100.0 * (double) goodAnswers / getValidationSamplesNumber() + "%");
+        System.out.println("correct : " + 100.0 * (double) goodAnswers / ValidationSamplesNumber + "%");
         System.out.println("now start entering your points (x, y) from 0.0 to 1.0:");
         while (true) {
             String xString, yString;
             xString = sc.next();
             yString = sc.next();
-            xString = xString.replace('.', ',');
-            yString = yString.replace('.', ',');
+            xString = xString.replace(',', '.');
+            yString = yString.replace(',', '.');
             double x = Double.parseDouble(xString), y = Double.parseDouble(yString);
             FeaturesVector obj = new FeaturesVector(2);
             obj.setFeature(0, x);
